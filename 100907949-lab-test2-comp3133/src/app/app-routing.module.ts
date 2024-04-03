80971a0f9d67e138d1e8d4cd4de749344bdaf1e7
TDotN <TDotN@DESKTOP-LAYZEEB>
2024-04-03 17:09:07 -0400

    completed lab test

---
+++ 100907949-lab-test2-comp3133/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MissionlistComponent } from './missionlist/missionlist.component';
+import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+
+const routes: Routes = [
+  { path: '', component: MissionlistComponent },
+  { path: 'missiondetails', component: MissiondetailsComponent },
+  { path: 'missiondetails/:flight_number', component: MissiondetailsComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
